e564f5bb206ffd5c5550fbdfd90ca7efd4e78a6c
Jesse Vincent <jesse@keyboard.io>
2017-05-23 12:31:57 -0700

    Add makefile rules to install arduino, so travis doesn't have to do it

--- build/rules.mk
+++ build/rules.mk
@@ -1,7 +1,13 @@
+TRAVIS_ARDUINO=arduino-1.6.11
+TRAVIS_ARDUINO_FILE = $(TRAVIS_ARDUINO)-linux64.tar.xz
 EXTRA_BUILDER_ARGS="-libraries ."
 
 all: build-all
 
+travis-install-arduino:
+	wget http://downloads.arduino.cc/$(TRAVIS_ARDUINO_FILE)
+	tar xf $(TRAVIS_ARDUINO_FILE)
+	ln -s $(TRAVIS_ARDUINO) arduino
 
 astyle:
 	find . -type f -name \*.cpp |xargs -n 1 astyle --style=google
