07ae4d24215d0db64439c2eec3e7087484531892
Jesse Vincent <jesse@keyboard.io>
2017-05-30 00:32:56 -0700

    I'm not very good at Shell

--- build/arduino.mk
+++ build/arduino.mk
@@ -3,7 +3,7 @@ TRAVIS_ARDUINO_FILE = $(TRAVIS_ARDUINO)-linux64.tar.xz
 TRAVIS_ARDUINO_PATH ?= $(shell pwd)/$(TRAVIS_ARDUINO)
 
 travis-install-arduino:
-	if [! -d "$(TRAVIS_ARDUINO_PATH)" ]; then \
+	if [ ! -d "$(TRAVIS_ARDUINO_PATH)" ]; then \
 		wget http://downloads.arduino.cc/$(TRAVIS_ARDUINO_FILE); \
        		tar xf $(TRAVIS_ARDUINO_FILE); \
 	fi
