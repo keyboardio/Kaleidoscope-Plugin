e0f7a26775e0226aed859d98be40295d0ead66e4
Jesse Vincent <jesse@keyboard.io>
2017-05-30 01:07:48 -0700

    Prettify output

--- build/arduino.mk
+++ build/arduino.mk
@@ -1,9 +1,7 @@
 TRAVIS_ARDUINO=arduino-1.8.2
 TRAVIS_ARDUINO_FILE = $(TRAVIS_ARDUINO)-linux64.tar.xz
 TRAVIS_ARDUINO_PATH ?= $(shell pwd)/$(TRAVIS_ARDUINO)
+TRAVIS_ARDUINO_DOWNLOAD_URL = http://downloads.arduino.cc/$(TRAVIS_ARDUINO_FILE)
 
 travis-install-arduino:
-	if [ ! -d "$(TRAVIS_ARDUINO_PATH)" ]; then \
-		wget http://downloads.arduino.cc/$(TRAVIS_ARDUINO_FILE); \
-       		tar xf $(TRAVIS_ARDUINO_FILE); \
-	fi
+	if [ ! -d "$(TRAVIS_ARDUINO_PATH)" ]; then wget $(TRAVIS_ARDUINO_DOWNLOAD_URL); tar xf $(TRAVIS_ARDUINO_FILE); fi
