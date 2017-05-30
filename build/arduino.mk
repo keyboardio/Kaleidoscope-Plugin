cc674b41a2ad843e22b1e1bf290e41e01fcecf6b
Jesse Vincent <jesse@keyboard.io>
2017-05-30 00:24:23 -0700

    try to only install arduino if it doesn't exist

--- build/arduino.mk
+++ build/arduino.mk
@@ -3,5 +3,7 @@ TRAVIS_ARDUINO_FILE = $(TRAVIS_ARDUINO)-linux64.tar.xz
 TRAVIS_ARDUINO_PATH ?= $(shell pwd)/$(TRAVIS_ARDUINO)
 
 travis-install-arduino:
-	wget http://downloads.arduino.cc/$(TRAVIS_ARDUINO_FILE)
-	tar xf $(TRAVIS_ARDUINO_FILE)
+	if [! -d "$(TRAVIS_ARDUINO_PATH) ]; then \
+		wget http://downloads.arduino.cc/$(TRAVIS_ARDUINO_FILE); \
+       		tar xf $(TRAVIS_ARDUINO_FILE); \
+	fi
